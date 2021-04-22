9fd6b7109a9d46cd4e6d4fc5114c479cd238fc90
kotaesaki <jmkamy552@gmail.com>
2021-04-22 20:24:51 +0900

    bootstrap導入

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -56,7 +56,9 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  config.assets.enabled = true
   config.assets.debug = true
+  config.assets.digest = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
